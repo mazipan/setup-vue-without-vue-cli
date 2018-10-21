6f295da631de0306fccb24930e5e1cb802452842
Irfan Maulana <irfan.maulana@bizzy.co.id>
2018-10-21 11:07:29 +0700

    init parcel :package:

---
+++ parcel/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app" class="app">
+
+    <h1>Welcome to Vue</h1>
+    <h2>{{ message }}</h2>
+
+    <footer>
+      <p>Copyright © 2018 <a href="https://mazipanneh.com/blog/">Irfan Maulana</a>, All Rights Reserved.</p>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      message: 'Hello World!!!'
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
+
