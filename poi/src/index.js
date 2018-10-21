0c6ed839054c30c2951491788972c653c92572ee
Irfan Maulana <irfan.maulana@bizzy.co.id>
2018-10-21 11:12:18 +0700

    init commit poi :sunglasses:

---
+++ poi/src/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import App from './App.vue';
+
+/* global process */
+if (process.env.NODE_ENV !== 'production') {
+  Vue.config.devtools = true;
+}
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+});
